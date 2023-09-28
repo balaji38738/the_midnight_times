571702dac3077e9cf75a69e1ccfade246781444e
Balaji Ijjapwar <bijjapwar123@gmail.com>
2023-09-28 12:31:42 +0530

    user app

---
+++ user/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegistrationForm, UserUpdateForm
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'Your account has been created, You can now login!')
+            return redirect('login')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'user/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        if u_form.is_valid():
+            u_form.save()
+            messages.success(request, f'Your profile has been updated!')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+    context = {
+        'u_form': u_form,
+    }
+    return render(request, 'users/profile.html', context)
